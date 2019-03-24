888ce5955b5c850ac4eaa87453716beca83df8cf
surajp92 <surajp92@vt.edu>
2019-03-23 22:33:43 -0500

    thomas algorithm code completed

--- C Tutorials/thomas_alg.c
+++ C Tutorials/thomas_alg.c
@@ -11,17 +11,97 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+
+void ThomasAlgorithm(int N, double *b, double *a, double *c, double *x, double *q)
+{
+	int i;
+	
+	double *l = malloc( sizeof(double) * N);
+	double *d = malloc( sizeof(double) * N);
+	double *u = malloc( sizeof(double) * N);
+	double *y = malloc( sizeof(double) * N);
+	
+	// c++ syntax
+	//double *l, *u, *d, *y;
+	//l = new double[N];
+	//u = new double[N];
+	//d = new double[N];
+	//y = new double[N];
+	
+	/* LU decomposition A = LU*/
+	d[0] = a[0];
+	u[0] = c[0];
+	for (i=0;i<N-2;i++)
+	{
+		l[i] = b[i]/d[i];
+		d[i+1] = a[i+1]-l[i]*u[i];
+		u[i+1] = c[i+1];
+	}
+	l[N-2] = b[N-2]/d[N-2];
+	d[N-1] = a[N-1] - l[N-2]*u[N-2];
+	
+	/* Forward substitution Ly = q */
+	y[0] = q[0];
+	for (i=1;i<N;i++)
+	{
+		y[i] = q[i]-l[i-1]*y[i-1];
+	}
+	
+	/* Backward substitution Ux = y */
+	x[N-1]  = q[N-1]/d[N-1];
+	for (i = N-2; i>=0; i--)
+	{
+		x[i] = (y[i]-u[i]*x[i+1])/d[i];
+	}
+	
+	free( l );
+	free( u );
+	free( d );
+	free( y );
+	
+	// c++ syntax
+	//delete[] l;
+	//delete[] d;
+	//delete[] u;	
+	//delete[] y;
+	return;
+}
 
 int main()
 {
     int N;
-    double a[N], b[N], c[N], x[N], q[N];
-    int i;
     N = 5;
-    for(i=0;i<N;i++)
+    double a[N], b[N], c[N], x[N], q[N];
+    double *p;
+    p = &b[0];
+    int i;
+    a[0] = 1.0;
+    c[0] = 0.0;
+    q[0] = 100.0; // left boundary
+    x[0] = 0.0;
+    for(i=0; i<N-2; i++)
     {
-        printf(i);
+        b[i] = 1.0;
+        a[i+1] = -2.0;
+        c[i+1] = 1.0;
+        q[i+1] = 0.0;
+        x[i+1] = 0.0;// initial condition
     }
+    // printf("pointer = %g", p[1]);
+    b[N-2] = 0.0;
+    a[N-1] = 1.0;
+    q[N-1] = 50.0; // right boundary
+    x[N-1] = 0.0;
+    
+    ThomasAlgorithm(N, a, b, c, x, q);
+    // print arrays a,b,c
+    for (i = 0;i<N; i++)
+    {
+		printf("a[%d] = %g\n", i, x[i]);
+		printf("b[%d] = %g\n", i, q[i]);
+		//printf("c[%d] = %g\n", i, c[i]);
+	}
     return 0;
 }
 
