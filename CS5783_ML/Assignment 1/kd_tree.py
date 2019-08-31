3ad1577c94fc8b87c72fb943ddf09319cbd4931f
surajp92 <33787868+surajp92@users.noreply.github.com>
2019-08-30 22:44:24 -0500

    Update kd_tree.py
    nearest neighbour kdtree simple approach added

--- CS5783_ML/Assignment 1/kd_tree.py
+++ CS5783_ML/Assignment 1/kd_tree.py
@@ -34,7 +34,6 @@ def find_nearest_neighbour(train_data, test_data):
     
     for data in train_data:
         current_distance = distane_squared(data, test_data)
-        print(data, " ", current_distance)
         
         if nearest_distance == None or current_distance < nearest_distance:
             nearest_distance = current_distance
@@ -45,16 +44,16 @@ def find_nearest_neighbour(train_data, test_data):
 train_data = np.random.randn(5,2)
 test_data = np.array([0.5,0.5])
 
-find_nearest_neighbour(train_data, test_data)
+nearest_neighbour, nearest_distance = find_nearest_neighbour(train_data, test_data)
+print(nearest_neighbour)
 
 #%%
-k = 3
-train_data = np.random.randn(5,3)
+k = 2
 
 def build_kdtree(train_data, depth = 0):
     n = (train_data.shape[0])
    
-    if n <= 2:
+    if n <= 1:
         return train_data
     
     axis = depth%k
@@ -71,6 +70,35 @@ def build_kdtree(train_data, depth = 0):
 s_data = (build_kdtree(train_data))
 pprint.pprint(s_data)
                       
-
 #%%
+test_data = test_data.reshape((1,2))
+
+def kdtree_nearest_neighbour(kdtree, test_data, depth = 0):
+    k = 2
+    axis = depth % k
+    
+    if type(kdtree) != dict:
+        return kdtree
+    
+    print(test_data[:,axis])
+    
+    if test_data[:,axis] < kdtree['data']:
+        print('left')
+        next_branch = kdtree['left']
+        pprint.pprint(next_branch)
+    else:
+        print('right')
+        next_branch = kdtree['right']
+        pprint.pprint(next_branch)
+
+    kdtree_nearest_neighbour(next_branch, test_data, depth + 1)
+    
+nn = kdtree_nearest_neighbour(s_data, test_data)
+print(nn)
+
+
+
+
+
+
     
