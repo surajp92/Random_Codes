9483f68c09ea52644ad7b70c2b5ac892ddc97135
surajp92 <33787868+surajp92@users.noreply.github.com>
2019-09-21 15:45:00 -0500

    Update assn2.py
    assignment 2 problem 1

--- CS5783_ML/Assignment 2/assn2.py
+++ CS5783_ML/Assignment 2/assn2.py
@@ -27,14 +27,18 @@ training_images = training_images.reshape(60000,-1)
 training_labels = np.array(training_labels)
 training_labels = training_labels.reshape(60000,-1)
 
-training_images = training_images/255
-
-a = training_images[3].reshape(28,28)
-plt.imshow(a)
+cat, counts = np.unique(training_labels, return_counts=True)
+theta_kd = np.zeros((cat.shape[0],training_images.shape[1]))
+for c in cat:
+    current_x = training_images[training_labels[:,0] == c]
+    theta_kd[c,:] = np.sum(current_x,axis=0,keepdims=True)/counts[c]
 
 #%%
-training_images[training_images[:,:]>0.1] = 1
-training_images[training_images[:,:]<0.1] = 0
+training_images[training_images[:,:]>=1] = 1
+training_images[training_images[:,:]<1] = 0
 
-a = training_images[0].reshape(28,28)
+
+
+#%%
+a = training_images[1].reshape(28,28)
 plt.imshow(a)
