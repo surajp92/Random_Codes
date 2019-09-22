7894622d710d82310fb853efc6dcc12cd23953a1
surajp92 <surajp92@vt.edu>
2019-09-22 16:02:23 -0500

    nearest neighbour problem 3 completed

--- CS5783_ML/Assignment 2/assn2.py
+++ CS5783_ML/Assignment 2/assn2.py
@@ -8,6 +8,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils import *
 from scipy.stats import multivariate_normal as mvn
+from numpy import linalg as LA
+from sklearn.model_selection import KFold
+import random
+from numpy.random import seed
+seed(222)
 
 #%%
 train_images, train_labels, test_images, test_labels = get_data()
@@ -34,10 +39,9 @@ for j in range(n):
         prob0 = 1-theta_kd[c][mask2]
         p[j,c] = np.sum(np.log(prob1)) + np.sum(np.log(prob0)) + np.log(prior[c])
 
-cl = np.argmax(p,axis=1)
-z = test_labels[test_labels[:,0] == cl[:]]
+cl1 = np.argmax(p,axis=1)
+z1 = test_labels[test_labels[:,0] == cl1[:]]
 
-    
 #%%
 # problem 2
 mean_kd = np.zeros((cat.shape[0],train_images.shape[1]))
@@ -53,6 +57,93 @@ p = np.zeros((n,k))
 for c in cat:
     p[:,c] = mvn.logpdf(test_images,mean=mean_kd[c,:], cov=var_kd[c,:])
 
-cl = np.argmax(p,axis=1)
-z = test_labels[test_labels[:,0] == cl[:]]
+cl2 = np.argmax(p,axis=1)
+z2 = test_labels[test_labels[:,0] == cl2[:]]
+
+
+#%%
+def problem3():
+    test_index_set = np.arange(600).reshape(5,-1)
+    train_index_set = np.zeros((5,480))
+    train_index_set[0,:] = np.arange(120,600)
+    train_index_set[1,:] = np.hstack((np.arange(120),np.arange(240,600)))
+    train_index_set[2,:] = np.hstack((np.arange(240),np.arange(360,600)))
+    train_index_set[3,:] = np.hstack((np.arange(360),np.arange(480,600)))
+    train_index_set[4,:] = np.arange(480)
+    train_index_set = train_index_set.astype(int)
     
+    train_images_new, train_label_new, test_data, test_label = get_data_problem3()
+    
+    rand_mask = np.array(random.sample(range(600), 600))
+    train_images_shf =train_images_new[rand_mask]
+    train_labels_shf =train_label_new[rand_mask]
+    
+    accuracy = np.zeros((5,5))
+    l = 0
+    Klist = np.array([1,3,5,7,9])
+    for K in Klist:
+        p = 0
+        for j in range(5):
+            train_data = train_images_shf[train_index_set[j,:]]
+            train_label = train_labels_shf[train_index_set[j,:]]
+            val_data = train_images_shf[test_index_set[j,:]]
+            val_label = train_labels_shf[test_index_set[j,:]]
+            
+            pred_label = np.zeros((val_data.shape[0],1))
+            
+            for i in range(val_data.shape[0]):
+                distance = LA.norm(train_data-val_data[i,:], axis=1, keepdims=True)    
+                indices = np.argsort(distance,axis=0)
+                nn_indices = indices[:K].flatten()
+                data_labels = train_label[nn_indices]
+                unique, counts = np.unique(data_labels, return_counts=True)
+                pred_label[i] = unique[np.argmax(counts)]
+            
+            accuracy[l,p] = len(val_label[pred_label==val_label])/len(val_label)
+            p = p+1
+        l = l+1
+    
+    mean_performance = np.mean(accuracy,axis=1,keepdims=True)    
+    K_model = Klist[np.argmax(mean_performance,axis=0)]
+    max_accuracy = np.max(mean_performance)
+    
+    print("Best K = ", K_model)
+    print("Mean validation accuracy = ", max_accuracy)
+    
+    test_pred_label = np.zeros((test_data.shape[0],1))
+    for i in range(test_data.shape[0]):
+        distance = LA.norm(train_images_shf-test_data[i,:], axis=1, keepdims=True)    
+        indices = np.argsort(distance,axis=0)
+        nn_indices = indices[:K_model[0]].flatten()
+        data_labels = train_labels_shf[nn_indices]
+        unique, counts = np.unique(data_labels, return_counts=True)
+        test_pred_label[i] = unique[np.argmax(counts)]
+    
+    test_accuracy = len(test_label[test_pred_label==test_label])/len(test_label)
+    print("Test accuracy = ", test_accuracy)
+    
+    plot_mask = test_pred_label[:] == test_label[:]
+    c1, ic1 = np.argmax(plot_mask[:50]), np.argmin(plot_mask[:50])
+    c2, ic2 = np.argmax(plot_mask[50:100])+50, np.argmin(plot_mask[50:100])+50
+    c7, ic7 = np.argmax(plot_mask[100:])+100, np.argmin(plot_mask[100:])+100
+    
+    plot_problem3(test_data, c1, ic1, c2, ic2, c7, ic7)
+
+problem3()
+
+
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CS5783_ML/Assignment 2/utils.py
+++ CS5783_ML/Assignment 2/utils.py
@@ -53,3 +53,97 @@ def get_data():
     test_images[test_images[:,:]<1] = 0
     
     return training_images, training_labels, test_images, test_labels
+
+#%%
+def get_data_nt():
+    training_images_file = open('train-images.idx3-ubyte','rb')
+    training_images = training_images_file.read()
+    training_images_file.close()
+    training_images = bytearray(training_images)
+    training_images = training_images[16:]
+    
+    training_labels_file = open('train-labels.idx1-ubyte','rb')
+    training_labels = training_labels_file.read()
+    training_labels_file.close()
+    training_labels = bytearray(training_labels)
+    training_labels = training_labels[8:]
+    
+    test_images_file = open('t10k-images.idx3-ubyte','rb')
+    test_images = test_images_file.read()
+    test_images_file.close()
+    test_images = bytearray(test_images)
+    test_images = test_images[16:]
+    
+    test_labels_file = open('t10k-labels.idx1-ubyte','rb')
+    test_labels = test_labels_file.read()
+    test_labels_file.close()
+    test_labels = bytearray(test_labels)
+    test_labels = test_labels[8:]
+    
+    training_images = np.array(training_images, dtype=int)
+    training_images = training_images.reshape(60000,-1)
+    
+    training_labels = np.array(training_labels,dtype=int)
+    training_labels = training_labels.reshape(60000,-1)
+    
+    test_images = np.array(test_images, dtype=int)
+    test_images = test_images.reshape(10000,-1)
+    
+    test_labels = np.array(test_labels, dtype=int)
+    test_labels = test_labels.reshape(10000,-1)
+    
+    return training_images, training_labels, test_images, test_labels
+
+#%%
+def get_data_problem3():
+    train_images, train_labels, test_images, test_labels = get_data_nt()    
+    set1,label1 = train_images[train_labels[:,0]==1], train_labels[train_labels[:,0]==1]
+    set2,label2 = train_images[train_labels[:,0]==2], train_labels[train_labels[:,0]==2]
+    set7,label7 = train_images[train_labels[:,0]==7], train_labels[train_labels[:,0]==7]
+    set1 = set1[np.random.randint(set1.shape[0],size=200)]
+    set2 = set2[np.random.randint(set1.shape[0],size=200)]
+    set7 = set7[np.random.randint(set1.shape[0],size=200)]
+    label1 = label1[:200,:]
+    label2 = label2[:200,:]
+    label7 = label7[:200,:]
+    train_images_new = np.vstack((set1,set2,set7))
+    train_label_new = np.vstack((label1,label2,label7))
+    
+    set1,label1 = test_images[test_labels[:,0]==1], test_labels[test_labels[:,0]==1]
+    set2,label2 = test_images[test_labels[:,0]==2], test_labels[test_labels[:,0]==2]
+    set7,label7 = test_images[test_labels[:,0]==7], test_labels[test_labels[:,0]==7]
+    set1 = set1[np.random.randint(set1.shape[0],size=50)]
+    set2 = set2[np.random.randint(set1.shape[0],size=50)]
+    set7 = set7[np.random.randint(set1.shape[0],size=50)]
+    label1 = label1[:50,:]
+    label2 = label2[:50,:]
+    label7 = label7[:50,:]
+    test_images_new = np.vstack((set1,set2,set7))
+    test_label_new = np.vstack((label1,label2,label7))
+    
+    return train_images_new, train_label_new, test_images_new, test_label_new
+
+#%%
+def plot_problem3(test_data, c1, ic1, c2, ic2, c7, ic7):
+    print("See plots")
+    fig, axs = plt.subplots(2,3,figsize=(8,6))
+    
+    axs[0,0].imshow(test_data[c1,:].reshape(28,28))
+    axs[0,0].set_title('Correctly classified 1')
+    axs[1,0].imshow(test_data[ic1,:].reshape(28,28))
+    axs[1,0].set_title('Incorrectly classified 1')
+    
+    axs[0,1].imshow(test_data[c2,:].reshape(28,28))
+    axs[0,1].set_title('Correctly classified 2')
+    axs[1,1].imshow(test_data[ic2,:].reshape(28,28))
+    axs[1,1].set_title('Correctly classified 2')
+    
+    axs[0,2].imshow(test_data[c7,:].reshape(28,28))
+    axs[0,2].set_title('Correctly classified 7')
+    axs[1,2].imshow(test_data[ic7,:].reshape(28,28))
+    axs[1,2].set_title('Correctly classified 7')
+    
+    plt.subplots_adjust(wspace=0, hspace=0)
+    
+    fig.tight_layout()
+    plt.show()
