42386786c4b0764f79e527c9c193afe688d9b640
Suraj <surajp92@vt.edu>
2019-11-17 15:05:31 -0600

    hmm backward pending

--- CS5783_ML/Assignment 4/assn4.py
+++ CS5783_ML/Assignment 4/assn4.py
@@ -66,82 +66,82 @@ def gp_kernel_e(xtrain,ytrain,xtest,ytest,param):
     return mu, stdv
     
 #%%
-def problem1():
-    data = np.loadtxt('crash.txt')
-    m,n = data.shape
-    
-    
-    mindata = np.min(data,axis=0)
-    data = data - mindata
-    maxdata = np.max(data,axis=0)
-    data = data/maxdata
-    
-    folds = 5
-    cv_results_se = np.zeros((101,6))
-    cv_results_e = np.zeros((101,6))
-    params = np.linspace(0.01,0.5,101)
-    foldsize = int(data.shape[0]/folds)
-    
-    random_indices = np.arange(data.shape[0])
-    np.random.shuffle(random_indices)
-    
-    for j in range(params.shape[0]):
-        param = params[j]
-        cv_results_se[j,0] = param
-        
-        for i in range(5):
-            randind_train = np.vstack((random_indices[:i*foldsize].reshape(-1,1),
-                                       random_indices[(i+1)*foldsize:].reshape(-1,1)))
-            
-            randind_test = random_indices[i*foldsize:(i+1)*foldsize]
-            
-            xtrain = data[randind_train.reshape(-1,),0].reshape(-1,1)
-            ytrain = data[randind_train.reshape(-1,),1].reshape(-1,1)
-            
-            xtest = data[randind_test.reshape(-1,),0].reshape(-1,1)
-            ytest = data[randind_test.reshape(-1,),1].reshape(-1,1)
-            
-            mu_se,stdev_se = gp_kernel_se(xtrain,ytrain,xtest,ytest,param)
-            mse = np.mean((np.square(mu_se - ytest.reshape(-1,))))
-            cv_results_se[j,i+1] = mse
-            
-            mu_e,stdev_e = gp_kernel_e(xtrain,ytrain,xtest,ytest,param)
-            mse = np.mean((np.square(mu_e - ytest.reshape(-1,))))
-            cv_results_e[j,i+1] = mse
-    
-    mean_mse_se = np.mean(cv_results_se[:,1:],axis=1)        
-    opt_param_se = params[np.argmin(mean_mse_se)]
-    
-    mean_mse_e = np.mean(cv_results_e[:,1:],axis=1)        
-    opt_param_e = params[np.argmin(mean_mse_e)]
-    
-    
-    xtrain, ytrain = data[:,0].reshape(-1,1), data[:,1].reshape(-1,1)
-    xtest = np.linspace(0,1,101)
-    xtest = xtest.reshape(-1,1)
-    
-    mu_se,stdev_se = gp_kernel_se(xtrain,ytrain,xtest,ytest,opt_param_se)
-    mu_e,stdev_e = gp_kernel_e(xtrain,ytrain,xtest,ytest,opt_param_e)
-    
-    print('Squeared exponential')
-    print('Optimal sigma = ', format(opt_param_se, '.3f'))
-    fig, ax = plt.subplots(nrows=1,ncols=1,figsize=(10,4))
-    ax.plot(data[:,0],data[:,1],marker='o',label='True data')
-    ax.plot(xtest,mu_se,marker='o',label='Gaussian process')
-    ax.legend()
-    ##plt.gca().fill_between(xtest.flat, mu-1*stdv, mu+1*stdv, color="#dddddd")
-    plt.show()
-    
-    print('Exponential')
-    print('Optimal sigma = ', format(opt_param_e, '.3f'))
-    fig, ax = plt.subplots(nrows=1,ncols=1,figsize=(10,4))
-    ax.plot(data[:,0],data[:,1],marker='o',label='True data')
-    ax.plot(xtest,mu_e,marker='o',label='Gaussian process')
-    ax.legend()
-    ##plt.gca().fill_between(xtest.flat, mu-1*stdv, mu+1*stdv, color="#dddddd")
-    plt.show()
+#def problem1():
+data = np.loadtxt('crash.txt')
+m,n = data.shape
 
-problem1()
+
+mindata = np.min(data,axis=0)
+data = data - mindata
+maxdata = np.max(data,axis=0)
+data = data/maxdata
+
+folds = 5
+cv_results_se = np.zeros((101,6))
+cv_results_e = np.zeros((101,6))
+params = np.linspace(0.01,0.5,101)
+foldsize = int(data.shape[0]/folds)
+
+random_indices = np.arange(data.shape[0])
+np.random.shuffle(random_indices)
+
+for j in range(params.shape[0]):
+    param = params[j]
+    cv_results_se[j,0] = param
+    
+    for i in range(5):
+        randind_train = np.vstack((random_indices[:i*foldsize].reshape(-1,1),
+                                   random_indices[(i+1)*foldsize:].reshape(-1,1)))
+        
+        randind_test = random_indices[i*foldsize:(i+1)*foldsize]
+        
+        xtrain = data[randind_train.reshape(-1,),0].reshape(-1,1)
+        ytrain = data[randind_train.reshape(-1,),1].reshape(-1,1)
+        
+        xtest = data[randind_test.reshape(-1,),0].reshape(-1,1)
+        ytest = data[randind_test.reshape(-1,),1].reshape(-1,1)
+        
+        mu_se,stdev_se = gp_kernel_se(xtrain,ytrain,xtest,ytest,param)
+        mse = np.mean((np.square(mu_se - ytest.reshape(-1,))))
+        cv_results_se[j,i+1] = mse
+        
+        mu_e,stdev_e = gp_kernel_e(xtrain,ytrain,xtest,ytest,param)
+        mse = np.mean((np.square(mu_e - ytest.reshape(-1,))))
+        cv_results_e[j,i+1] = mse
+
+mean_mse_se = np.mean(cv_results_se[:,1:],axis=1)        
+opt_param_se = params[np.argmin(mean_mse_se)]
+
+mean_mse_e = np.mean(cv_results_e[:,1:],axis=1)        
+opt_param_e = params[np.argmin(mean_mse_e)]
+
+
+xtrain, ytrain = data[:,0].reshape(-1,1), data[:,1].reshape(-1,1)
+xtest = np.linspace(0,1,101)
+xtest = xtest.reshape(-1,1)
+
+mu_se,stdev_se = gp_kernel_se(xtrain,ytrain,xtest,ytest,opt_param_se)
+mu_e,stdev_e = gp_kernel_e(xtrain,ytrain,xtest,ytest,opt_param_e)
+
+print('Squeared exponential')
+print('Optimal sigma = ', format(opt_param_se, '.3f'))
+fig, ax = plt.subplots(nrows=1,ncols=1,figsize=(10,4))
+ax.plot(data[:,0],data[:,1],marker='o',label='True data')
+ax.plot(xtest,mu_se,marker='o',label='Gaussian process')
+ax.legend()
+##plt.gca().fill_between(xtest.flat, mu-1*stdv, mu+1*stdv, color="#dddddd")
+plt.show()
+
+print('Exponential')
+print('Optimal sigma = ', format(opt_param_e, '.3f'))
+fig, ax = plt.subplots(nrows=1,ncols=1,figsize=(10,4))
+ax.plot(data[:,0],data[:,1],marker='o',label='True data')
+ax.plot(xtest,mu_e,marker='o',label='Gaussian process')
+ax.legend()
+##plt.gca().fill_between(xtest.flat, mu-1*stdv, mu+1*stdv, color="#dddddd")
+plt.show()
+
+#problem1()
 
 #%%
 test_images_file = open('t10k-images-idx3-ubyte','rb')
@@ -158,90 +158,174 @@ test_labels = test_labels[8:]
 
 test_images = np.array(test_images)
 test_images = test_images.reshape(10000,-1)
+test_images = test_images.astype(np.float64)
+
 
 test_labels = np.array(test_labels)
 test_labels = test_labels.reshape(10000,-1)
 
-#test_images[test_images[:,:]>=1] = 1
-#test_images[test_images[:,:]<1] = 0
-
-K = 10
-# part 1
-centroid = np.random.rand(10,784)*255
-
-# part 2
-
-#part 3
-centroid = np.zeros((K,test_images.shape[1]))
-for k in range(K):
-    centroid[k,:] = test_images[test_labels.reshape(-1,)==k][0]
-
-# part 4
-
-
-centroid_temp = np.zeros((10,784))
-
-dist_mat = np.zeros((test_images.shape[0],10))
-
-residual = 1
-
-n = 0
-
-#%%
-while residual > 0.001:
-    for k in range(K):
-        b = test_images - centroid[k,:]
-        dist_mat[:,k] = LA.norm(b,axis=1)**2
+for q in range(4):
+    # part 1
+    if q == 0:
+        K = 10
+        random_indices = np.random.randint(0,10000,K)
+        centroid = test_images[random_indices] #np.random.rand(10,784)*255
     
-    ind = np.argmin(dist_mat,axis=1)
-
+    # part 2
+    if q == 1:
+        K = 10
+        centroid = np.zeros((K,test_images.shape[1]))
+        centroid[0,:] = test_images[np.random.randint(0,10000,1)]
+        for k in range(1,K):
+            b = test_images - centroid[k-1,:]
+            distkmpp = LA.norm(b,axis=1)**2
+            centroid[k,:] = test_images[np.argmax(distkmpp)]
+    
+    #part 3
+    if q == 2:
+        K = 10
+        centroid = np.zeros((K,test_images.shape[1]))
+        for k in range(K):
+            centroid[k,:] = test_images[test_labels.reshape(-1,)==k][0]
+    
+    # part 4
+    if q == 3:
+        K = 3
+        centroid = np.zeros((K,test_images.shape[1]))
+        
+        centroid[0,:] = test_images[np.random.randint(0,10000,1)]
+        for k in range(1,K):
+            b = test_images - centroid[k-1,:]
+            distkmpp = LA.norm(b,axis=1)**2
+            centroid[k,:] = test_images[np.argmax(distkmpp)]
+    
+    centroid_temp =  np.zeros((K,test_images.shape[1]))
+    dist_mat = np.zeros((test_images.shape[0],K))
+    residual = 1
+    n = 0
+    
+    while residual  > 0.001:
+        for k in range(K):
+            b = test_images - centroid[k,:]
+            dist_mat[:,k] = LA.norm(b,axis=1)**2
+        
+        ind = np.argmin(dist_mat,axis=1)
+    
+        for k in range(K):
+            temp = test_images[ind == k]
+            #print(k, " ", temp.shape)
+            if temp.size != 0:
+                centroid_temp[k,:] = np.average(temp,axis=0)
+        
+        residual= LA.norm(centroid - centroid_temp)
+        centroid[:,:] = centroid_temp[:,:]
+        print('.',end =" ")
+        #print(n, " res = ", residual)
+        n = n+1
+    
+    J = 0
     for k in range(K):
         temp = test_images[ind == k]
-        if temp.size != 0:
-            centroid_temp[k,:] = np.average(temp,axis=0)
+        b = temp - centroid[k,:]
+        J = J + LA.norm(b)**2
     
-    residual = LA.norm(centroid - centroid_temp)
-    centroid[:,:] = centroid_temp[:,:]
-    print('.',end =" ")
-    #print(n, " res = ", residual)
-    n = n+1
+    print(n, " Iterations ", J )
+    
+    if q == 3:
+        unique, counts = np.unique(ind, return_counts=True)
 
-print(n, " Iterations")
-
-J = 0
-for k in range(K):
-    temp = test_images[ind == k]
-    b = temp - centroid[k,:]
-    J = J + LA.norm(b)**2
-
-fig, axs = plt.subplots(nrows=2,ncols=5,figsize=(10,4))
-ax = axs.flat
-
-for k in range(K):
-    ax[k].imshow(centroid[k].reshape(28,28))
-    ax[k].set_xticks([])
-    ax[k].set_yticks([])
-
-fig.tight_layout()
-plt.show()
-
-#%%    
-unique, counts = np.unique(ind, return_counts=True)
-unique1, counts1 = np.unique(test_labels, return_counts=True)
-
-comp = test_labels == ind.reshape(-1,1)
-u,c = np.unique(comp, return_counts=True)
-
-dist = LA.norm(centroid[0,:]-test_images[0,:], 2)**2
-
+        fig, axs = plt.subplots(nrows=1,ncols=3,figsize=(6,3))
+        ax = axs.flat
+        
+        for k in range(K):
+            ax[k].imshow(centroid[k].reshape(28,28))
+            ax[k].set_xticks([])
+            ax[k].set_yticks([])
+        
+        fig.tight_layout()
+        print('Output for problem 2 part 4')
+        plt.show()
+        
+        fig, axs = plt.subplots(nrows=3,ncols=2,figsize=(6,9))
+        ax = axs.flat
+        p = 0
+        
+        for i in range(3):
+            for j in range(2):
+                ax[p].imshow(test_images[ind==unique[i]][j].reshape(28,28))
+                ax[p].set_xticks([])
+                ax[p].set_yticks([])
+                ax[p].set_title('Cluster '+str(i))
+                p = p+1
+        fig.tight_layout()
+        print('Output for problem 2 part '+str(q+1))
+        plt.show()
+        
+    else:        
+        fig, axs = plt.subplots(nrows=2,ncols=5,figsize=(10,4))
+        ax = axs.flat
+        
+        for k in range(K):
+            ax[k].imshow(centroid[k].reshape(28,28))
+            ax[k].set_xticks([])
+            ax[k].set_yticks([])
+        
+        fig.tight_layout()
+        print('Output for problem 2 part '+str(q+1))
+        plt.show()
 
 #%% problem 3
+zval = np.array([0,1]) #Latent variable [0=Fair, 1=Loaded]
+xval = np.array([1,2,3,4,5,6]) #Obervations
+
+px_z = np.array([[1/6, 1/6, 1/6, 1/6, 1/6, 1/6], [0.1, 0.1, 0.1, 0.1, 0.1, 0.5]])
+pz_z = np.array([[0.95, 0.05],[0.1,0.9]])                
+
+Nsteps = 1000
+xtrue = np.zeros(Nsteps)
+ztrue = np.zeros(Nsteps)
+
+ztrue[0] = 0
+px = px_z[int(ztrue[0])]
+xtrue[0] = np.random.choice(xval, 1, p=px)
+
+for i in range(1,Nsteps):
+   
+   pz = pz_z[int(ztrue[i-1])]
+   ztrue[i] = np.random.choice(zval, 1, p=pz)
+   
+   px = px_z[int(ztrue[i])]
+   xtrue[i] = np.random.choice(xval, 1, p=px)
 
 
+#%%
+# Transition Probabilities
+a = np.array(((0.95, 0.05), (0.1, 0.9)))
+ 
+# Emission Probabilities
+b = np.array(((1/6, 1/6, 1/6, 1/6, 1/6, 1/6), (0.1, 0.1, 0.1, 0.1, 0.1, 0.5)))
 
+# Equal Probabilities for the initial distribution
+initial_distribution = np.array((0.5,0.5))
 
+def forward(V, a, b, initial_distribution):
+    alpha = np.zeros((V.shape[0], a.shape[0]))
+    alpha[0, :] = initial_distribution * b[:, int(V[0]-1)]
+    
+    alpha[0, :] = alpha[0,:]/np.sum(alpha[0,:])
+    
+    for t in range(1, V.shape[0]):
+        for j in range(a.shape[0]):
+            alpha[t, j] = alpha[t - 1].dot(a[:, j]) * b[j, int(V[t]-1)]
+        alpha[t,:]  = alpha[t,:]/np.sum(alpha[t,:])
+        
+    return alpha
+ 
+alpha = forward(xtrue, a, b, initial_distribution)
 
-
-
-
-
+fig, ax = plt.subplots(nrows=1,ncols=1,figsize=(12,4))
+ax.plot(ztrue,'k',label='Actual loaded dice')
+ax.plot(alpha[:,1],'r',label='Probability of loaded dice')
+ax.legend(loc=2)
+##plt.gca().fill_between(xtest.flat, mu-1*stdv, mu+1*stdv, color="#dddddd")
+plt.show()
