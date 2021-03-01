b030e0f932fd7c1cdf255f6b87cd933192efbbd0
surajp92 <surajp92@vt.edu>
2021-03-01 13:25:51 -0600

    fps

--- Fast_Poisson _Solver_Python/fps_dirichlet.py
+++ Fast_Poisson _Solver_Python/fps_dirichlet.py
@@ -12,6 +12,7 @@ from scipy import integrate
 from scipy import linalg
 import matplotlib.pyplot as plt 
 from scipy.fftpack import fft2, ifft2
+import time
 
 font = {'family' : 'Times New Roman',
         'size'   : 14}	
@@ -29,23 +30,24 @@ def fps(nx, ny, dx, dy, f):
     kx = np.empty(nx)
     ky = np.empty(ny)
     
-    for i in range(nx):
-        kx[i] = hx*np.float64(i)
-    
-    for i in range(ny):
-        ky[i] = hy*np.float64(i)
+    kx[:] = hx*np.float64(np.arange(0, nx))
+
+    ky[:] = hy*np.float64(np.arange(0, ny))
     
     # small number to avoid division by 0
     kx[0] = epsilon
     ky[0] = epsilon
     
+    kx, ky = np.meshgrid(np.cos(kx), np.cos(ky), indexing='ij')
+    
     data = np.empty((nx,ny), dtype='complex128')
     data1 = np.empty((nx,ny), dtype='complex128')
     
     # create data using the dource term as the real part and 0.0 as the imaginary part
-    for i in range(nx):
-        for j in range(ny):
-            data[i,j] = complex(f[i,j],0.0)
+    data[:,:] = np.vectorize(complex)(f[0:nx,0:ny],0.0)
+#    for i in range(nx):
+#        for j in range(ny):
+#            data[i,j] = complex(f[i,j],0.0)
        
     a = pyfftw.empty_aligned((nx,ny),dtype= 'complex128')
     b = pyfftw.empty_aligned((nx,ny),dtype= 'complex128')
@@ -55,40 +57,68 @@ def fps(nx, ny, dx, dy, f):
     
     # compute the fourier transform
     e = fft_object(data)
-    #e = pyfftw.interfaces.scipy_fftpack.fft2(data)
+#    e = np.fft.fft2(data)
+#    e = pyfftw.interfaces.scipy_fftpack.fft2(data)
     
     e[0,0] = 0.0
+    data1[:,:] = e[:,:]/(aa + bb*kx[:,:] + cc*ky[:,:])
     
     # the donominator is based on the scheme used for discrtetizing the Poisson equation
-    for i in range(nx):
-        for j in range(ny):
-            data1[i,j] = e[i,j]/(aa + bb*np.cos(kx[i]) + cc*np.cos(ky[j]))
+#    for i in range(nx):
+#        for j in range(ny):
+#            data1[i,j] = e[i,j]/(aa + bb*np.cos(kx[i]) + cc*np.cos(ky[j]))
     
     # compute the inverse fourier transform
     u = np.real(fft_object_inv(data1))
+#    u = np.fft.ifft2(data1)
+    
     return u    
     
 #%%
 # 
-nx = 128
-ny = 128
-x = np.linspace(0.0,2.0*np.pi,nx+1)
-y = np.linspace(0.0,2.0*np.pi,ny+1)
-dx = 2.0*np.pi/(nx)
-dy = 2.0*np.pi/(ny)
+nx = 256
+ny = 512
+
+x_l = 0.0
+x_r = 1.0
+y_b = 0.0
+y_t = 1.0
+
+dx = (x_r-x_l)/nx
+dy = (y_t-y_b)/ny
+
+x = np.linspace(x_l, x_r, nx+1)
+y = np.linspace(y_b, y_t, ny+1)
+
+#x = np.linspace(0.0,2.0*np.pi,nx+1)
+#y = np.linspace(0.0,2.0*np.pi,ny+1)
+#dx = 2.0*np.pi/(nx)
+#dy = 2.0*np.pi/(ny)
 
 ue = np.empty((nx+1,ny+1))
 f = np.empty((nx+1,ny+1))
 
+km = 16.0
+c1 = (1.0/km)**2
+c2 = -8.0*np.pi**2
+
 # test case Ue = sin(3x) + cos(2y); f = -9sin(3x) - 4cos(2y)
 for i in range(nx+1):
     for j in range(ny+1):
-        ue[i,j] = np.sin(3.0*x[i]) + np.cos(2.0*y[j])
-        f[i,j] = -9.0*np.sin(3.0*x[i]) -4.0*np.cos(2.0*y[j])
+        ue[i,j] = np.sin(2.0*np.pi*x[i])*np.sin(2.0*np.pi*y[j]) +         c1*np.sin(km*2.0*np.pi*x[i])*np.sin(km*2.0*np.pi*y[j])
+        
+        f[i,j] = c2*np.sin(2.0*np.pi*x[i])*np.sin(2.0*np.pi*y[j]) +     c2*np.sin(km*2.0*np.pi*x[i])*np.sin(km*2.0*np.pi*y[j])
+        
+#        ue[i,j] = np.sin(3.0*x[i]) + np.cos(2.0*y[j])
+#        f[i,j] = -9.0*np.sin(3.0*x[i]) -4.0*np.cos(2.0*y[j])
 
 u = np.zeros((nx+1, ny+1))
+
+start = time.time()
 # fps subroutine skips the last row and last column since the boundary condition is periodic
 u[:nx, :ny] = fps(nx, ny, dx, dy, f)
+print('T = ', time.time() - start)
+print(np.linalg.norm(u-ue)/np.sqrt((nx*ny)))
 
 # extend boundary of the domain
 u[:,ny] = u[:,0]
@@ -99,9 +129,9 @@ u[nx,:] = u[0,:]
 fig, axs = plt.subplots(1,2,sharey=True,figsize=(9,5))
 
 cs = axs[0].contourf(x,y,ue.T, 120, cmap = 'jet', interpolation='bilinear')
-axs[0].text(0.4, -0.1, 'Exact', transform=axs[0].transAxes, fontsize=16, fontweight='bold', va='top')
+axs[0].text(0.4, -0.1, 'Exact', transform=axs[0].transAxes, fontsize=16,  va='top')
 cs = axs[1].contourf(x,y,u.T, 120, cmap = 'jet', interpolation='bilinear')
-axs[1].text(0.4, -0.1, 'Numerical', transform=axs[1].transAxes, fontsize=16, fontweight='bold', va='top')
+axs[1].text(0.4, -0.1, 'Numerical', transform=axs[1].transAxes, fontsize=16,  va='top')
 
 fig.tight_layout() 
 
@@ -111,4 +141,5 @@ cbar_ax = fig.add_axes([0.22, -0.05, 0.6, 0.04])
 fig.colorbar(cs, cax=cbar_ax, orientation='horizontal')
 plt.show()
 
-fig.savefig("fps.eps", bbox_inches = 'tight')
+#fig.savefig("fps.eps", bbox_inches = 'tight')
+
