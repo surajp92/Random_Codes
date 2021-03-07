d015a46e9cfba5576577ce6b92ad4b891a3eecb4
surajp92 <surajp92@vt.edu>
2021-03-07 11:31:37 -0600

    hw4 fem

--- MATH5563_FEA/Correct_Dimensions/hw4_problem1.py
+++ MATH5563_FEA/Correct_Dimensions/hw4_problem1.py
@@ -31,6 +31,9 @@ import matplotlib as mpl
 mpl.rcParams['text.usetex'] = True
 from matplotlib.lines import Line2D
 
+import sys
+orig_stdout = sys .stdout
+
 class pdePr1:
     def __init__(self):
         
@@ -61,81 +64,130 @@ class pdePr1:
 domain = [0,1]
 pde = pdePr1()
 bc = [1,1] # Dirichlet on left and Neumann on Right side
+error_list= []
+coarse = True # solving only for pd = 1 and N = 5 (h=1/5)
 
-fig, axs = plt.subplots(1,4,sharex=True,sharey=True,figsize=(12,6))
+if coarse:
+    fig, ax = plt.subplots(1,2,sharex=True,figsize=(12,5))
+    nc = 1
+    pd_list = [1]
+else:
+    fig, ax = plt.subplots(3,5,sharex=True,figsize=(18,15))
+    nc = 5
+    pd_list = [1,2,3]
+    
+axs = ax.flat
+count = 0
 
-for i in range(4):    
-    #%% 2 Generate Mesh and FEM
-    n = int(5*(2**i))
-    pd = 2
-    ng = 5
+for pd in pd_list:
+    file = open(f'./logs/hw4_problem1_fem_out_{pd}.log', 'w')
+    sys.stdout = file
     
-    mesh = genMesh1D(domain, n)
-    fem = genFEM1D(mesh, pd)
-    
-    #%% 3 Node type and DOF
-    fem.genBC1D(domain, bc)
-    
-    #%% 4 Generate Matrices abd Vectors
-    Mxx = globalMatrix1D(pde.a, mesh, fem, 1, fem, 1, ng)
-    M00 = globalMatrix1D(pde.c, mesh, fem, 0, fem, 0, ng)
-    f = globalVec1D(pde.f, mesh, fem, 0, ng)
-    
-    #%% 5 Dirichlet boundary condition
-    uD = pde.gD(fem.p)
-    uD[fem.dof] = 0.0
-    bxx = Mxx @ uD
-    b00 = M00 @ uD
-    
-    #%% 6 Neumann boundary condition
-    bN = np.zeros(np.shape(fem.p))
-    bN[fem.ptype == 2] = pde.gN(fem.p[fem.ptype == 2])
-    
-    #%% 7 Extract degree of freedom
-    ii,jj = np.meshgrid(fem.dof,fem.dof,indexing='ij')
-    A = Mxx[ii,jj] + M00[ii,jj]
-    b = f[fem.dof] + bN[fem.dof] -bxx[fem.dof] - b00[fem.dof]
-    
-    #%% 8 Solve Linear Sysetem
-    uDof = inv(A) @ b
-    uh = np.copy(uD)
-    uh[fem.dof] = uDof      
-    
-    #%%
-    
-    tu = pde.exactu(fem.p)
-    for k in range(n):
-        vert = mesh.p[mesh.t[k,:]]
-        x = np.linspace(vert[0], vert[1], 11)
+    for i in range(nc):    
+        #%% 2 Generate Mesh and FEM
+        n = int(5*(2**i))
+        ng = 5
         
-        uhK = uh[fem.t[k,:]]
-        u = evalFEfun1D(x, uhK, vert, pd, 0)
+        mesh = genMesh1D(domain, n)
+        fem = genFEM1D(mesh, pd)
         
-        axs[i].plot(x,pde.exactu(x),'k',lw=2)
-        axs[i].plot(x,u,'r--',lw=2)
+        #%% 3 Node type and DOF
+        fem.genBC1D(domain, bc)
         
-    axs[i].grid()
-    custom_lines = [Line2D([0], [0], color='k', lw=2),
-                        Line2D([0], [0], color='r',ls='--', lw=2)]
-    axs[i].legend(custom_lines, ['$u$','$Iu$'])
-    axs[i].set_xlim(domain)   
-    axs[i].set_xlabel('$x$')
+        #%% 4 Generate Matrices abd Vectors
+        Mxx = globalMatrix1D(pde.a, mesh, fem, 1, fem, 1, ng)
+        M00 = globalMatrix1D(pde.c, mesh, fem, 0, fem, 0, ng)
+        f = globalVec1D(pde.f, mesh, fem, 0, ng)
         
-    #%%
-    errL2, errKL2 = getErr1D(uh, pde.exactu, mesh, fem, ng, 0)
-    errH1, errKH2 = getErr1D(uh, pde.Du, mesh, fem, ng, 1)
-    
-    print('#----------------------------------------#')
-    print('n = %d' % (n))
-    print('L2 error:  %5.3e' % errL2)
-    print('H1 error:  %5.3e' % errH1)
-    if i>=1:
-        rateL2 = np.log(errL2_0/errL2)/np.log(2.0);
-        rateH1 = np.log(errH1_0/errH1)/np.log(2.0);
-        print('L2 order:  %5.3f' % rateL2)
-        print('H1 order:  %5.3f' % rateH1)   
-    
-    errL2_0 = errL2
-    errH1_0 = errH1
+        #%% 5 Dirichlet boundary condition
+        uD = pde.gD(fem.p)
+        uD[fem.dof] = 0.0
+        bxx = Mxx @ uD
+        b00 = M00 @ uD
+        
+        #%% 6 Neumann boundary condition
+        bN = np.zeros(np.shape(fem.p))
+        bN[fem.ptype == 2] = pde.gN(fem.p[fem.ptype == 2])
+        
+        #%% 7 Extract degree of freedom
+        ii,jj = np.meshgrid(fem.dof,fem.dof,indexing='ij')
+        A = Mxx[ii,jj] + M00[ii,jj]
+        b = f[fem.dof] + bN[fem.dof] -bxx[fem.dof] - b00[fem.dof]
+        
+        #%% 8 Solve Linear Sysetem
+        uDof = inv(A) @ b
+        uh = np.copy(uD)
+        uh[fem.dof] = uDof      
+        
+        #%%
+        
+        tu = pde.exactu(fem.p)
+        for k in range(n):
+            vert = mesh.p[mesh.t[k,:]]
+            x = np.linspace(vert[0], vert[1], 11)
+            
+            uhK = uh[fem.t[k,:]]
+            u = evalFEfun1D(x, uhK, vert, pd, 0)
+            
+            axs[count].plot(x,pde.exactu(x),'k',lw=2 )
+            axs[count].plot(x,u,'r--',lw=2)
+            if coarse:
+                axs[count+1].plot(x,pde.exactu(x) -u,'b',lw=2)
+            
+        axs[count].grid()
+        custom_lines = [Line2D([0], [0], color='k', lw=2),
+                            Line2D([0], [0], color='r',ls='--', lw=2)]
+        axs[count].legend(custom_lines, ['$u$','$Fu$'])
+        if coarse:
+            custom_lines = [Line2D([0], [0], color='b', lw=2)]
+            axs[count+1].legend(custom_lines, ['$u - Fu$'])
+        axs[count].set_xlim(domain)   
+        axs[count].set_xlabel('$x$')
+        axs[count].set_title(f'PD={pd}, $N$={n}')
+        
+        count = count + 1
+            
+        #%%
+        errL2, errKL2 = getErr1D(uh, pde.exactu, mesh, fem, ng, 0)
+        errH1, errKH2 = getErr1D(uh, pde.Du, mesh, fem, ng, 1)
+        
+        error_list.append([pd, n, errL2, errH1])
+        
+        print('#----------------------------------------#')
+        print('n = %d' % (n))
+        print('L2 error:  %5.3e' % errL2)
+        print('H1 error:  %5.3e' % errH1)
+        if i>=1:
+            rateL2 = np.log(errL2_0/errL2)/np.log(2.0);
+            rateH1 = np.log(errH1_0/errH1)/np.log(2.0);
+            print('L2 order:  %5.3f' % rateL2)
+            print('H1 order:  %5.3f' % rateH1)   
+        
+        errL2_0 = errL2
+        errH1_0 = errH1
 
 plt.show()
+if coarse:
+    fig.savefig('./logs/hw4_problem1_fem_u1.png', dpi=300, bbox_inches="tight")
+else:
+    fig.savefig('./logs/hw4_problem1_fem_u.png', dpi=300, bbox_inches="tight")
+    
+#%%
+if not coarse:
+    fig, axs = plt.subplots(1,2,sharex=True,figsize=(12,5))
+    error_list = np.array(error_list)
+    
+    for pd in [1,2,3]:
+        slice_ = error_list[:,0] == pd
+        axs[0].loglog(error_list[slice_, 1], error_list[slice_, 2],'s-', label=f'PD = {pd}')
+        axs[1].loglog(error_list[slice_, 1], error_list[slice_, 3], 's-', label=f'PD = {pd}')
+    
+    axs[0].set_ylim([1e-14,1e0])
+    axs[1].set_ylim([1e-14,1e2])
+    axs[0].set_xscale('log', basex=2)
+    axs[1].set_xscale('log', basex=2)
+    axs[0].legend()
+    axs[1].legend()
+    plt.show()
+    fig.tight_layout()
+    fig.savefig(f'./logs/hw4_problem1_fem.png', dpi=300)    
