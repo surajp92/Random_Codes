359be20a4ff548d97d8ee6b090835d1963e61976
surajp92 <pawarsuraj92@gmail.com>
2021-03-15 22:46:20 -0500

    fem2d code

---
+++ MATH5563_FEA/Correct_Dimensions/2D/genFem2D.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  9 21:22:24 2021
+
+@author: suraj
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+domain = [1.0,1.0]
+xl = domain[0]
+yl = domain[1]
+
+nx = 3
+ny = 3
+
+ne = (nx-1)*(ny-1)*2
+ne_x = (nx - 1)*2
+
+mesh_t = np.zeros((ne,3), dtype=int)
+
+i = np.arange(nx-1).reshape(1,-1) 
+j = np.arange(ny-1).reshape(-1,1) 
+
+ise = 2*np.arange(0,int(ne/2))
+# ise = ise.flatten()
+
+mesh_t[ise,0] = (i + j*nx).flatten()
+mesh_t[ise,1] = mesh_t[ise,0] + 1
+mesh_t[ise,2] = mesh_t[ise,0] + nx
+
+iso = ise + 1
+
+mesh_t[iso,0] = mesh_t[ise,2] + 1
+mesh_t[iso,1] = mesh_t[iso,0] - 1
+mesh_t[iso,2] = mesh_t[iso,0] - nx
+
+
+print(mesh_t)
+
+#%%
+nnodes = nx*ny
+mesh_p = np.zeros((nnodes,2))
+x = np.linspace(0,xl,nx)
+y = np.linspace(0,xl,ny)
+
+X, Y = np.meshgrid(x,y,indexing='ij')
+
+mesh_p[:,0] = np.reshape(X,[-1,],order='f')
+mesh_p[:,1] = np.reshape(Y,[-1,],order='f')
+
+#%%
+nedges = (nx-1)*(ny) + (ny-1)*nx + (nx-1)*(ny-1)
+
+n_p = nnodes + nedges
+fem_p = np.zeros((n_p,2))
+
+fem_p[:nnodes,:] = mesh_p 
+
+comb_list = [[0,1],[1,2],[0,2]]
+
+start = nnodes
+
+for list_ in comb_list:
+    aa = (mesh_p[mesh_t[:,list_[0]]] + mesh_p[mesh_t[:,list_[1]]])/2.0
+    aau, indices = np.unique(aa, axis=0, return_index=True)
+    
+    end = start+indices.shape[0]
+    fem_p[start:end,:] = aa[sorted(indices),:]
+    
+    start = end
+
+print(fem_p)
--- MATH5563_FEA/Correct_Dimensions/2D/genMesh2D.py
+++ MATH5563_FEA/Correct_Dimensions/2D/genMesh2D.py
@@ -10,8 +10,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #%%
-nx = 5
-ny = 5
+domain = [1.0,1.0]
+xl = domain[0]
+yl = domain[1]
+
+nx = 3
+ny = 3
 
 ne = (nx-1)*(ny-1)*2
 ne_x = (nx - 1)*2
@@ -21,7 +25,7 @@ t = np.zeros((ne,3), dtype=int)
 for j in range(ny-1):
     i1 = np.arange(nx-1) 
     ise = 2*i1 + j*ne_x
-    # print(is_)
+    print(i1, j, ise)
     
     t[ise,0] = i1 + j*nx
     t[ise,1] = t[ise,0] + 1
@@ -43,3 +47,62 @@ for j in range(ny-1):
 
 
 print(t)
+
+#%%
+
+t1 = np.zeros((ne,3), dtype=int)
+i = np.arange(nx-1).reshape(1,-1) 
+j = np.arange(ny-1).reshape(-1,1) 
+
+ise = 2*i + j*ne_x
+ise = ise.flatten()
+
+
+t1[ise,0] = (i + j*nx).flatten()
+t1[ise,1] = t1[ise,0] + 1
+t1[ise,2] = t1[ise,0] + nx
+
+iso = ise + 1
+
+t1[iso,0] = t1[ise,2] + 1
+t1[iso,1] = t1[iso,0] - 1
+t1[iso,2] = t1[iso,0] - nx
+
+
+print(t1)
+
+print(t - t1)
+
+#%%
+t1 = np.zeros((ne,3), dtype=int)
+i = np.arange(nx-1).reshape(1,-1) 
+j = np.arange(ny-1).reshape(-1,1) 
+
+ise = 2*np.arange(0,int(ne/2))
+# ise = ise.flatten()
+
+t1[ise,0] = (i + j*nx).flatten()
+t1[ise,1] = t1[ise,0] + 1
+t1[ise,2] = t1[ise,0] + nx
+
+iso = ise + 1
+
+t1[iso,0] = t1[ise,2] + 1
+t1[iso,1] = t1[iso,0] - 1
+t1[iso,2] = t1[iso,0] - nx
+
+
+print(t1)
+
+print(t - t1)
+
+#%%
+nn = nx*ny
+p = np.zeros((nn,2))
+x = np.linspace(0,xl,nx)
+y = np.linspace(0,xl,ny)
+
+X, Y = np.meshgrid(x,y,indexing='ij')
+
+p[:,0] = np.reshape(X,[-1,],order='f')
+p[:,1] = np.reshape(Y,[-1,],order='f')
