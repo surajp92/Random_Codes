a24bc3e85feda13f3a3d309027ced7ea838618a9
surajp92 <pawarsuraj92@gmail.com>
2021-04-07 22:14:04 -0500

    hw5

--- MAE6263_CFD/HW5/nonlinear_wave_equation.py
+++ MAE6263_CFD/HW5/nonlinear_wave_equation.py
@@ -13,16 +13,16 @@ font = {'size' : 16}
 plt.rc('font', **font)
 
 #%%
-a = 250.0
-l = 400.0
-tm = 1.0
-dx = 5.0
-m = 3
-dt_list = [0.02,0.01,0.005]
+a = 1.0
+l = 4.0
+tm = 1.8
+dx = 0.1
+m = 1
+dt_list = [0.1]
 u_all = []
 t_all = []
 x_all = []
-nt_list = 1.0/np.array(dt_list)
+nt_list = [0,6,12,18]
 
 for i in range(m):
     dt = dt_list[i]
@@ -36,16 +36,16 @@ for i in range(m):
     x = np.linspace(0,l,n+1)        
     t = np.linspace(0,tm,nt+1)
     X,T = np.meshgrid(x,t, indexing='ij')
-    u0 = 100.0*np.sin(np.pi*(x - 50.0)/60.0)
     
     k = 0
-    u[:,k] = u0
-    u[x <= 50.0,k] = 0.0
-    u[x >= 110.0,k] = 0.0
+    u[x <= 2.0,k] = 1.0
+    u[x >= 2.0,k] = 0.0
     
-    i = np.arange(n+1)
+    u[0,:] = 1.0
+    
+    i = np.arange(1,n)
     for k in range(1,nt+1):
-        u[i,k] = u[i,k-1] - c*(u[i,k-1] - u[i-1,k-1])
+        u[i,k] = 0.5*(u[i+1,k-1] + u[i-1,k-1]) - (0.5*dt/dx)*(0.5*u[(i+1),k-1]**2 - 0.5*u[i-1,k-1]**2)
     
     x_all.append(x)
     t_all.append(t)
@@ -58,21 +58,23 @@ plt.show()
 
 #%%
 fig,ax = plt.subplots(1,1,figsize=(10,5))
-for i in range(m):
-    x = x_all[i]
-    u = u_all[i]
-    ax.plot(x,u[:,int(nt_list[i]/2)],'o-',lw=2,label=f'c=${1.0/2**i}$')
+for i in range(4):
+    x = x_all[0]
+    u = u_all[0]
+    ax.plot(x,u[:,nt_list[i]],'o-',lw=2,label=f'$t={nt_list[i]*dt:0.2f}$')
 ax.legend()    
-ax.set_title('First-upwind explicit scheme')
+ax.set_title('Lax explicit scheme')
 ax.set_xlabel('$x$')
 ax.set_ylabel('$u$')
 plt.show()    
 
 #%%
+m = 2
+dt_list = [0.1,0.05]
 u_all = []
 t_all = []
 x_all = []
-nt_list = 1.0/np.array(dt_list)
+nt_list = [0,6,12,18]
 
 for i in range(m):
     dt = dt_list[i]
@@ -86,29 +88,29 @@ for i in range(m):
     x = np.linspace(0,l,n+1)        
     t = np.linspace(0,tm,nt+1)
     X,T = np.meshgrid(x,t, indexing='ij')
-    u0 = 100.0*np.sin(np.pi*(x - 50.0)/60.0)
     
     k = 0
-    u[:,k] = u0
-    u[x <= 50.0,k] = 0.0
-    u[x >= 110.0,k] = 0.0
+    u[x <= 2.0,k] = 1.0
+    u[x >= 2.0,k] = 0.0
     
-    i = np.arange(n+1)
+    u[0,:] = 1.0
+    
+    i = np.arange(1,n)
     for k in range(1,nt+1):
-        u[i,k] = u[i,k-1] - 0.5*c*(u[(i+1)%(n+1),k-1] - u[i-1,k-1]) + \
-        0.5*c**2*(u[(i+1)%(n+1),k-1] - 2.0*u[i,k-1] + u[i-1,k-1])
+        u[i,k] = 0.5*(u[i+1,k-1] + u[i-1,k-1]) - (0.5*dt/dx)*(0.5*u[(i+1),k-1]**2 - 0.5*u[i-1,k-1]**2)
     
     x_all.append(x)
     t_all.append(t)
     u_all.append(u)
-
+    
+#%%
 fig,ax = plt.subplots(1,1,figsize=(10,5))
-for i in range(m):
+for i in range(2):
     x = x_all[i]
     u = u_all[i]
-    ax.plot(x,u[:,int(nt_list[i]/2)],'o-',lw=2,label=f'c=${1.0/2**i}$')
+    ax.plot(x,u[:,-1],'o-',lw=2,label=f'$c={dt_list[i]/dx:0.2f}$')
 ax.legend()    
-ax.set_title('Lax-Wendroff explicit scheme')
+ax.set_title('Lax explicit scheme')
 ax.set_xlabel('$x$')
 ax.set_ylabel('$u$')
-plt.show()     
+plt.show()   
